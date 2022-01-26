76866303fe610d6c43ffc9721321342e85e94dc4
Guillaume <robert.guillaume70@laposte.net>
2022-01-26 13:16:42 +0100

    add index content  + icon h2

--- elements/footer.php
+++ elements/footer.php
@@ -1,4 +1,5 @@
 
 <script src="Js/app.js"></script>
+<script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require("elements/header.php"); ?>
 <section class="container">
     <div class="img-animed"><img src="images/coiffure à domicile.jpg" alt="Coiffure à domicile" class="img-medium" ></div>
     <div class="concept">
-                <h2>Le concept de la coiffure à domicile</h2>
+                <h2>Le concept de la coiffure à <span>domicile <span class="iconify" data-icon="maki:hairdresser"></span> </span></h2>
                 <p>Un rendez-vous de coiffure à domicile ne présente que des avantages puisque c'est l'artisan coiffeur
                     qui se déplace sur le lieu de votre choix.</p>
                 <p> Fini le déplacement et l'attente en salon. Plus besoin de faire garder les enfants ou ressortir
@@ -15,15 +15,43 @@ require("elements/header.php"); ?>
             </div>
 </section>
 
+<section class="container">
+<h2>Mais comment ça <span> marche ?<img class="iconify" src="images/43984-e63e6f.svg" ></span></h2>
 
+<div class="card-wrapper">
 
+<div class="card">
+    <span class="iconify" data-icon="clarity:car-solid"></span>
+    <h3>Zone de déplacement</h3>
+    <p>Je me déplace à 20km autour de Corre en Haute-Saône</p>
+    <p>Disponible du lundi au vendredi de 9h à 18h. Ainsi que le samedi de 9h à 12h.</p>
+</div>
 
+<div class="card">
+    <span class="iconify" data-icon="bi:calendar-week-fill"></span>                       
+    <h3>Prise de rendez vous</h3>
+    <p>Vous pouvez prendre rendez-vous par téléphone ou par SMS au 06.31.10.42.07.</p>
+    <p>Nous programmerons ensemble le jour et l'heure de votre rendez-vous.</p>
+</div>
 
+<div class="card">
+    <span class="iconify" data-icon="maki:hairdresser"></span>                    
+    <h3>Le jour-J</h3>
+    <p>Avant de commencer, j'effectue un diagnostic sur vos envies et vous conseille en fonction de votre style et de vos petits rituels capillaires. Ensuite ? </p>
+    <p>Glissez vos pieds sous la table, japporte tout le matériel nécessaire.</p>
+</div>
 
+<div class="card">
+<span class="iconify" data-icon="octicon:comment-discussion-24"></span>      
+    <h3>Partagez</h3>
+    <p>Si vous êtes satisfait de mon travail, n'hésitez pas à partager votre nouvelle tête et votre expérience de la coiffure à domicile auprès de vos proches.</p>
+</div>
 
+</div>
+</section>
 
-
-
-
+<section class="container">
+<h2>Suis-je dans la zone de <span>déplacement ? <img src="images/iconFichier 1.svg" class="iconify rotate-45"></span></h2>
+</section>
 
 <?php require("elements/footer.php"); ?>
